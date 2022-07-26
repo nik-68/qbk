8213c257397053150897b1aab537fd9e861b0c37
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-26 15:59:51 +0300

    Update qbk.py

--- qbk.py
+++ qbk.py
@@ -15,6 +15,21 @@ def dos():
 while True:
  threading.Thread(target=dos).start()
 
+except (KeyboardInterrupt, SystemExit):
+                print "CTRL+C received. Killing all workers"
+                for worker in self.workersQueue:
+                    try:
+                        if DEBUG:
+                            print "Killing worker {0}".format(worker.name)
+                        #worker.terminate()
+                        worker.stop()
+                    except Exception, ex:
+                        pass # silently ignore
+                if DEBUG:
+                    raise
+                else:
+                    pass
+	
 if __name__ == '__main__':
 	starturl() # questo fa startare la prima funzione del programma, che a sua volta ne starta un altra, poi un altra, fino ad arrivare all'attacco.
 Footer
